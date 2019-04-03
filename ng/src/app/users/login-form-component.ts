76a36328d971444efba7068aeb36b09fac5109fb
Abdulmalek Al-Gahmi <aalgahmi@cs.weber.edu>
2019-04-03 09:22:09 -0600

    After apr 3 class

--- ng/src/app/users/login-form-component.ts
+++ ng/src/app/users/login-form-component.ts
@@ -1,9 +1,34 @@
 import { Component } from "@angular/core";
 import { User } from '../models/user';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './login-form.component.html'
 })
 export class LoginFormComponent {
     user: User = new User()
+
+    constructor(private toastr: ToastrService, 
+        private authService: AuthenticationService,
+        private router : Router){}
+        
+    onSubmit(f: NgForm) {
+        if (f.valid) {
+            this.authService.login(this.user).subscribe(res => {
+                this.toastr.success("Successfully logged in.")
+                this.router.navigate(['/books'])
+            }, error => {
+                this.toastr.error('Unable to log in! Contact admin.')
+            })
+
+           
+        } else {
+            for (let c in f.controls) {
+                f.controls[c].markAsDirty()
+            }
+        }
+    }
 }
